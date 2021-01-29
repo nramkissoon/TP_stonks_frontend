19f039ccaf1a02429181a785cac3763d7df5983b
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-01-29 11:27:24 -0500

    refactor consts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,16 @@
-export const greenTextColor = '#8AFF8C'
-export const redTextColor = '#FE5D5D'
-export const whiteTextColor = '#FFF'
+export const greenTextColor = '#8AFF8C';
+export const redTextColor = '#FE5D5D';
+export const whiteTextColor = '#FFF';
+
+export const ENV_PROD = 'production';
+export const ENV_DEV = 'development';
+
+// update interval in milliseconds
+export const UPDATE_INTERVAL_PROD = 5000; 
+export const UPDATE_INTERVAL_DEV = 15000;
+
+export const ACCOUNT_DATA_API_URL = 'http://localhost:8080/data/account';
+
+//update times in minutes
+export const UPDATE_START_TIME = 9 * 60;
+export const UPDATE_END_TIME = 16 * 60 + 30;
